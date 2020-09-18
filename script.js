255f6a79f5083ca226c1c180ecaed0cc15537eb8
Squich <quichaud.stephane@gmail.com>
2020-09-18 22:29:43 +0200

    formatage

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 /* Select DOM elements */
 
-const container = document.getElementById('container');
-const date = document.getElementById('date');
+const container = document.getElementById("container");
+const date = document.getElementById("date");
 
-const btnAdd = document.getElementById('btn-add');
-const btnCloseAdd = document.getElementById('btn-close-add');
+const btnAdd = document.getElementById("btn-add");
+const btnCloseAdd = document.getElementById("btn-close-add");
 
-const empty = document.getElementById('empty');
-const list = document.getElementById('list');
-const btnDeleteAll = document.getElementById('btn-delete-all');
+const empty = document.getElementById("empty");
+const list = document.getElementById("list");
+const btnDeleteAll = document.getElementById("btn-delete-all");
 
-const formAdd = document.getElementById('form');
-const formTitle = document.getElementById('form-title');
-const formName = document.getElementById('form-name');
-const formDate = document.getElementById('form-date');
-const formRecipe = document.getElementById('form-recipe');
+const formAdd = document.getElementById("form");
+const formTitle = document.getElementById("form-title");
+const formName = document.getElementById("form-name");
+const formDate = document.getElementById("form-date");
+const formRecipe = document.getElementById("form-recipe");
 const formOpened = document.querySelectorAll('input[name="opened"]');
-const formOpenedYes = document.getElementById('opened-yes');
-const formOpenedNo = document.getElementById('opened-no');
-const btnSubmit = document.getElementById('btn-submit');
+const formOpenedYes = document.getElementById("opened-yes");
+const formOpenedNo = document.getElementById("opened-no");
+const btnSubmit = document.getElementById("btn-submit");
 
 /* Variables */
 
 let listItems,
-  newItemName = '',
-  newItemDate = '',
-  newItemRecipe = '',
-  newItemOpened = 'false',
-  modifiedItemIndex = '';
+  newItemName = "",
+  newItemDate = "",
+  newItemRecipe = "",
+  newItemOpened = "false",
+  modifiedItemIndex = "";
 
 /* One day in milliseconds */
 
@@ -35,30 +35,30 @@ const d = 1000 * 60 * 60 * 24;
 
 /* Date formats */
 
-const formatDateLong = (date) => `${date.getFullYear()}-${date.getMonth() + 1 < 10 ? '0' : ''}${date.getMonth() + 1}-${date.getDate() < 10 ? '0' : ''}${date.getDate()}`;
-const formatDateShort = (str) => str.replace(/(\d{4})-(\d{2})-(\d{2})/g, '$3/$2');
+const formatDateLong = (date) => `${date.getFullYear()}-${date.getMonth() + 1 < 10 ? "0" : ""}${date.getMonth() + 1}-${date.getDate() < 10 ? "0" : ""}${date.getDate()}`;
+const formatDateShort = (str) => str.replace(/(\d{4})-(\d{2})-(\d{2})/g, "$3/$2");
 
 /* Generate startList */
 
 const generateDate = (num) => formatDateLong(new Date(new Date().getTime() + num));
 
 const startList = [
-  {name: 'Saumon', date: generateDate(-d * 9), recipe: '', opened: 'true'},
-  {name: 'Fromage rapé', date: generateDate(-d * 3), recipe: 'Omelette', opened: 'false'},
-  {name: 'Crème fraiche', date: generateDate(0), recipe: 'Chantilly', opened: 'false'},
-  {name: 'Saucisses', date: generateDate(d * 2), recipe: '', opened: 'true'},
-  {name: 'Lardons', date: generateDate(d * 2), recipe: 'Salade composée', opened: 'false'},
-  {name: 'Camembert', date: generateDate(d * 4), recipe: '', opened: 'false'},
-  {name: 'Lait', date: generateDate(d * 5), recipe: '', opened: 'false'},
-  {name: 'Pizza', date: generateDate(d * 7), recipe: '', opened: 'true'},
-  {name: 'Gnocchis', date: generateDate(d * 10), recipe: '', opened: 'false'}
+  {name: "Saumon", date: generateDate(-d * 9), recipe: "", opened: "true"},
+  {name: "Fromage rapé", date: generateDate(-d * 3), recipe: "Omelette", opened: "false"},
+  {name: "Crème fraiche", date: generateDate(0), recipe: "Chantilly", opened: "false"},
+  {name: "Saucisses", date: generateDate(d * 2), recipe: "", opened: "true"},
+  {name: "Lardons", date: generateDate(d * 2), recipe: "Salade composée", opened: "false"},
+  {name: "Camembert", date: generateDate(d * 4), recipe: "", opened: "false"},
+  {name: "Lait", date: generateDate(d * 5), recipe: "", opened: "false"},
+  {name: "Pizza", date: generateDate(d * 7), recipe: "", opened: "true"},
+  {name: "Gnocchis", date: generateDate(d * 10), recipe: "", opened: "false"}
 ];
 
 /* Generate list with listItems */
 
 function generateList() {
   getListItems();
-  list.innerHTML = '';
+  list.innerHTML = "";
   today();
   checkEmpty();
 
@@ -67,7 +67,7 @@ function generateList() {
     .forEach((item, i) => {
       /* Li */
 
-      const listItem = document.createElement('li');
+      const listItem = document.createElement("li");
       listItem.dataset.index = i;
       listItem.innerHTML = `
 			<span>
@@ -76,30 +76,30 @@ function generateList() {
 				<span class="name">${item.name}</span>
 			</span>
 			<span>
-				${item.recipe ? '<span class="recipe-container"><i class="icon-recipe fa fa-tags"></i></span>' : ''}
+				${item.recipe ? '<span class="recipe-container"><i class="icon-recipe fa fa-tags"></i></span>' : ""}
 				<i class="btn-edit fa fa-pencil"></i>
 				<i class="btn-delete fa fa-times"></i>
 			</span>`;
 
       /* Tooltips */
 
-      const tooltipDLC = document.createElement('div');
-      tooltipDLC.className = 'tooltip';
-      tooltipDLC.textContent = diff(item.date) + ' jr' + (item.opened === 'false' ? '' : ' (entamé)');
-      listItem.querySelector('.smiley-container').prepend(tooltipDLC);
+      const tooltipDLC = document.createElement("div");
+      tooltipDLC.className = "tooltip";
+      tooltipDLC.textContent = diff(item.date) + " jr" + (item.opened === "false" ? "" : " (entamé)");
+      listItem.querySelector(".smiley-container").prepend(tooltipDLC);
 
       if (item.recipe) {
-        const tooltipRecipe = document.createElement('div');
-        tooltipRecipe.className = 'tooltip';
+        const tooltipRecipe = document.createElement("div");
+        tooltipRecipe.className = "tooltip";
         tooltipRecipe.textContent = item.recipe;
-        listItem.querySelector('.recipe-container').append(tooltipRecipe);
+        listItem.querySelector(".recipe-container").append(tooltipRecipe);
       }
 
       /* Smiley */
 
-      const smiley = document.createElement('i');
-      smiley.className = 'smiley fa ' + chooseSmiley(item.opened === 'false' || !item.opened ? diff(item.date) : Infinity) + ' ' + colorizeSmiley(diff(item.date));
-      listItem.querySelector('.smiley-container').prepend(smiley);
+      const smiley = document.createElement("i");
+      smiley.className = "smiley fa " + chooseSmiley(item.opened === "false" || !item.opened ? diff(item.date) : Infinity) + " " + colorizeSmiley(diff(item.date));
+      listItem.querySelector(".smiley-container").prepend(smiley);
 
       list.appendChild(listItem);
     });
@@ -109,23 +109,23 @@ function generateList() {
 
 /* Set and Get from Local Storage */
 
-const setListItems = () => localStorage.setItem('listItems', JSON.stringify(listItems));
-const getListItems = () => (listItems = JSON.parse(localStorage.getItem('listItems')) || startList);
+const setListItems = () => localStorage.setItem("listItems", JSON.stringify(listItems));
+const getListItems = () => (listItems = JSON.parse(localStorage.getItem("listItems")) || startList);
 
 /* Display a message if the list is empty */
 
-const checkEmpty = () => (listItems.length ? (empty.classList.remove('active'), btnDeleteAll.classList.add('active')) : (empty.classList.add('active'), btnDeleteAll.classList.remove('active')));
+const checkEmpty = () => (listItems.length ? (empty.classList.remove("active"), btnDeleteAll.classList.add("active")) : (empty.classList.add("active"), btnDeleteAll.classList.remove("active")));
 
 /* Delete ​​variables values and close the window */
 
 const clearAndClose = () => {
-  modifiedItemIndex = '';
-  newItemName = '';
-  newItemDate = '';
-  newItemRecipe = '';
-  newItemOpened = 'false';
+  modifiedItemIndex = "";
+  newItemName = "";
+  newItemDate = "";
+  newItemRecipe = "";
+  newItemOpened = "false";
   formAdd.reset();
-  container.classList.remove('add-active');
+  container.classList.remove("add-active");
 };
 
 /* Generate the current date */
@@ -138,37 +138,37 @@ const diff = (date) => Math.ceil((new Date(date) - new Date()) / d);
 
 /* Choose and colorize the icon of the elements according to the remaining time and the Opened setting */
 
-const chooseSmiley = (num) => (num < 0 ? 'fa-frown-o' : num < 4 ? 'fa-meh-o' : num === Infinity ? 'fa-exclamation' : 'fa-smile-o');
-const colorizeSmiley = (num) => (num < 0 ? 'black' : num < 2 ? 'red' : num < 4 ? 'orange' : 'green');
+const chooseSmiley = (num) => (num < 0 ? "fa-frown-o" : num < 4 ? "fa-meh-o" : num === Infinity ? "fa-exclamation" : "fa-smile-o");
+const colorizeSmiley = (num) => (num < 0 ? "black" : num < 2 ? "red" : num < 4 ? "orange" : "green");
 
 /* Check the radio buttons */
 
-const checkIfOpened = () => (newItemOpened === 'true' ? (formOpenedYes.checked = true) : (formOpenedNo.checked = true));
+const checkIfOpened = () => (newItemOpened === "true" ? (formOpenedYes.checked = true) : (formOpenedNo.checked = true));
 
 /* Display Panel Add */
 
-btnAdd.addEventListener('click', () => {
-  formTitle.innerHTML = 'Ajouter un produit';
+btnAdd.addEventListener("click", () => {
+  formTitle.innerHTML = "Ajouter un produit";
   btnSubmit.innerHTML = '<i class="fa fa-plus"></i>Ajouter';
   checkIfOpened();
-  container.classList.add('add-active');
+  container.classList.add("add-active");
 });
-btnCloseAdd.addEventListener('click', clearAndClose);
+btnCloseAdd.addEventListener("click", clearAndClose);
 
 /* Events on the icons of each item in the list */
 
-list.addEventListener('click', (e) => {
+list.addEventListener("click", (e) => {
   const index = e.target.parentElement.parentElement.dataset.index;
 
   /* Activate the tooltip for remaining days or the tooltip for the specified recipe */
-  if (e.target.classList.contains('smiley') || e.target.classList.contains('icon-recipe')) {
-    e.target.classList.add('active');
-    window.setTimeout(() => e.target.classList.remove('active'), 1000);
+  if (e.target.classList.contains("smiley") || e.target.classList.contains("icon-recipe")) {
+    e.target.classList.add("active");
+    window.setTimeout(() => e.target.classList.remove("active"), 1000);
   }
 
   /* Edit one item in the list */
-  if (e.target.classList.contains('btn-edit')) {
-    formTitle.innerHTML = 'Modifier un produit';
+  if (e.target.classList.contains("btn-edit")) {
+    formTitle.innerHTML = "Modifier un produit";
     btnSubmit.innerHTML = '<i class="fa fa-pencil"></i>Modifier';
 
     newItemName = listItems[index].name;
@@ -178,17 +178,17 @@ list.addEventListener('click', (e) => {
 
     formName.value = listItems[index].name;
     formDate.value = listItems[index].date;
-    formRecipe.value = listItems[index].recipe || '';
+    formRecipe.value = listItems[index].recipe || "";
     checkIfOpened();
 
-    container.classList.add('add-active');
+    container.classList.add("add-active");
     modifiedItemIndex = index;
   }
 
   /* Delete one item in the list */
 
-  if (e.target.classList.contains('btn-delete')) {
-    if (window.confirm('Êtes-vous sûr de vouloir supprimer ce produit ?')) {
+  if (e.target.classList.contains("btn-delete")) {
+    if (window.confirm("Êtes-vous sûr de vouloir supprimer ce produit ?")) {
       listItems.splice(index, 1);
       setListItems();
       generateList();
@@ -198,8 +198,8 @@ list.addEventListener('click', (e) => {
 
 /* Delete the whole list */
 
-btnDeleteAll.addEventListener('click', () => {
-  if (window.confirm('Êtes-vous sûr de vouloir supprimer toute la liste ?')) {
+btnDeleteAll.addEventListener("click", () => {
+  if (window.confirm("Êtes-vous sûr de vouloir supprimer toute la liste ?")) {
     listItems.splice(0, listItems.length);
     setListItems();
     generateList();
@@ -208,17 +208,17 @@ btnDeleteAll.addEventListener('click', () => {
 
 /* Add item to list */
 
-formName.addEventListener('keyup', () => (newItemName = formName.value.trim()));
-formName.addEventListener('change', () => (newItemName = formName.value.trim()));
-formDate.addEventListener('change', () => (newItemDate = formDate.value));
-formRecipe.addEventListener('keyup', () => (newItemRecipe = formRecipe.value.trim()));
-formRecipe.addEventListener('change', () => (newItemRecipe = formRecipe.value.trim()));
-formOpened.forEach((btn) => btn.addEventListener('change', () => (newItemOpened = btn.value)));
+formName.addEventListener("keyup", () => (newItemName = formName.value.trim()));
+formName.addEventListener("change", () => (newItemName = formName.value.trim()));
+formDate.addEventListener("change", () => (newItemDate = formDate.value));
+formRecipe.addEventListener("keyup", () => (newItemRecipe = formRecipe.value.trim()));
+formRecipe.addEventListener("change", () => (newItemRecipe = formRecipe.value.trim()));
+formOpened.forEach((btn) => btn.addEventListener("change", () => (newItemOpened = btn.value)));
 
-btnSubmit.addEventListener('click', (e) => {
+btnSubmit.addEventListener("click", (e) => {
   e.preventDefault();
-  if (newItemName === '' || newItemDate === '') {
-    alert('Vous devez renseigner le nom et la DLC du produit à ajouter.');
+  if (newItemName === "" || newItemDate === "") {
+    alert("Vous devez renseigner le nom et la DLC du produit à ajouter.");
   } else {
     if (modifiedItemIndex) listItems.splice(modifiedItemIndex, 1);
     listItems.push({
